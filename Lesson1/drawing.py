aa3a5e61b6d9c06bc889f008a01e1f28132ba97a
pzivich <pzivich@live.unc.edu>
2021-07-09 08:03:31 -0400

    lesson1 updates

--- Lesson1/drawing.py
+++ Lesson1/drawing.py
@@ -1,61 +1,127 @@
 ######################################################################################################################
-# Lesson 3: Some Basic Drawing / Art
+# Lesson 1: Some Basic Drawing / Art
 #
-# Paul Zivich (2021/6/8)
+# Paul Zivich (2021/7/9)
 ######################################################################################################################
 
-import turtle
+# This week we will learn how to do some basic drawing using the python module: turtle. We will use the module to
+#   create some pictures. The pictures will draw when we run the code.
 
-sh = turtle.Turtle()
+# Learning objectives:
+#       0: python modules
+#       1: for-loops
+#       2: lists (a new object type!)
+
+# Modules: modules are sets of functions that we can load and use.
+#   This is easier to use more complex functions that aren't immediately available
+#   in python. When we installed python, it came with a basic set of modules. We
+#   will be using more of these modules as we continue to go through. First, we will
+#   use the `turtle` module to make some pictures
+# Turtle is one of the basic modules that comes with python. It features a simple
+#   animated / drawing library.
+
+import turtle  # loads the turtle module so we can use it (put this at the top of the script)
+
+# to access the turtle module, we need to specify turtle in the code
+# then we can use functions within the turtle module
+shelly = turtle.Turtle()  # this statement opens the turtle module and the Turtle function
 
 # Drawing a basic square
-sh.forward(100)
-sh.right(90)
-sh.forward(100)
-sh.right(90)
-sh.forward(100)
-sh.right(90)
-sh.forward(100)
-sh.right(90)
+shelly.forward(100)  # tells the turtle to go forward 100 steps
+shelly.right(90)  # tells the turtle to turn 90 degrees to the right
+shelly.forward(100)  # tells the turtle to go forward 100 steps
+shelly.right(90)
+shelly.forward(100)
+shelly.right(90)
+shelly.forward(100)
+shelly.right(90)
 
-# Thats a lot of steps. Lets do a loop instead.
-sh.reset()  # Resets the turtle
+shelly.reset()  # Resets the turtle
+
+# That is an annoying way to draw a square though. We have a series of repeated directions. Therefore, if we could
+#   repeat the same directions multiple times, our code could be simpler. We will do so by using for-loops
+
+#####################
+# for loops
+
+# When we want to do a procedure a bunch of times we can use a `for loop`.  Below is a basic for loop. What does this
+#   output? Also what is the `range` function?
 for i in range(4):
-    sh.forward(100)
-    sh.right(90)
+    print(i)
 
-sh.color("red")
+# Here is a better way to draw a square using a for-loop
 for i in range(4):
-    sh.forward(100)
-    sh.right(90)
+    shelly.forward(100)
+    shelly.right(90)
 
-sh.reset()
+# Let's change shelly's color for our next set of drawings
+shelly.color("red")
 
-turtle.bgcolor("black")
-turtle.speed("fastest")
+# Let's say we wanted to draw multiple squares now. We can also do that using nested for-loops
+# there are now 2 loops, the outer loop and the inner loop!
+for j in range(4):
+    shelly.penup()  # lifts up the pen (so no line is draw)
+    shelly.forward(110)
+    shelly.right(90)
+    shelly.pendown()  # sets the pen back down to draw
+    for i in range(4):  # drawing a square
+        shelly.forward(50)
+        shelly.right(90)
 
-sh.color("white")
-for i in range(20):
-    sh.forward(5*i)
-    sh.right(90)
+shelly.reset()  # Resets the turtle
 
-sh.reset()
+# Some additional options
+turtle.bgcolor("black")  # set the background color
+turtle.speed("fastest")  # sets the turtle speed (this is the fastest)
 
-sh.hideturtle()
-sh.color("white")
+# Drawing a simple labyrinth! We can do extra things with the loop indicator
+shelly.color("white")
+for i in range(1, 21):
+    shelly.forward(5*i)
+    shelly.right(90)
 
-for i in range(36):
-    sh.circle(100)
-    sh.right(10)
+shelly.reset()  # Resets the turtle
 
+# some other options!
+shelly.hideturtle()  # Hide the turtle indicator
+shelly.color("white")  # set the turtle color
+shelly.circle(100)  # Draw a circle with diameter of 100
+
+#####################
+# lists
+
+#   Lists are a new object type. They hold collections of other objects in a particular
+#   order. The following is an example of a list of 6 different string objects.
+#   Lists are useful for storing multiple objects then querying those objects. Also
+#   lists allow for a bunch of different object types (you can have a list of strings,
+#   integers, and floats with no issues. You can even have a list of lists of lists of ...
 color_list = ["red", "yellow", "blue", "orange", "green", "red"]
-for n in range(36):
-    #sh.begin_fill()
-    for i in range(6):
-        sh.color(color_list[i])
-        sh.forward(100)
-        sh.left(60)
-    #sh.end_fill()
-    sh.right(10)
 
-turtle.done()
+# you can also make lists of lists of lists. Lists are a very useful object and we will
+#   use them lots in future projects
+
+# We can access the objects in the list via the following
+print(color_list[0])  # first item in the list
+print(color_list[3])  # 4th item in the list
+print(color_list[-2])  # second last item in the list
+
+# You can also multiply lists! Give it a try and see what happens
+
+#####################
+# loops with lists
+
+# Sometimes we want to do a bunch of processes again and again. Here we use a loop of loops
+#   to draw a series of hexagons!
+for n in range(36):
+    # shelly.begin_fill()
+    for i in range(6):
+        shelly.color(color_list[i])
+        shelly.forward(100)
+        shelly.left(60)
+    # shelly.end_fill()
+    shelly.right(10)
+
+turtle.done()  # Tells turtle we are done creating graphics (be nice to the turtle!)
+# will also keep the turtle open until we click the 'X'
+
+
---
+++ Lesson1/homework1.py
@@ -0,0 +1,34 @@
+####################################################################################################################
+# Homework 1: art with turtle
+#
+# Name:
+####################################################################################################################
+
+# For this homework, create a piece of art using the turtle library. You should use the turtle library to draw some
+#   picture. In your code, use for-loops and lists to help out (code smarter, not harder!). I don't have any
+#   explicit requirements (besides encouraging you to use the new parts of code we learner), so be creative with
+#   what you make for next week!
+
+###########################################################
+# Here are some useful functions with the turtle library:
+
+# Color in the space the turtle draws around
+# shelly.begin_fill("red")  # starts filling in the area with the color
+# shelly.end_fill()  # ends the fill
+
+# Tell the turtle to go to a specific point (10, 10) in this case
+#   the starting point (origin) for the turtle is 0, 0
+# shelly.goto(10, 10)  # tells the turtle to go to 10, 10
+# shelly.home()  # special function that tells turtle to go to 0, 0
+
+# Draw a dot (with a width of 30)
+# shelly.dot(30)  # draws a large dot at current location
+
+# Changes the pen size (line width) for the turtle
+# shelly.pensize(3)  # changes the line width
+
+# Stop drawing a line
+# shelly.penup()  # picks up the pen
+# shelly.pendown()  # puts the pen back down
+
+
